9ffa5219e7ca4f0fa7428d2b59668f1677e709c6
Jose Carlos <rodjosecarlos15@gmail.com>
2024-02-07 14:56:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button mShowDialogBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowDialogBtn = findViewById(R.id.show_dialog_btn);
+        mShowDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new
+                        MaterialAlertDialogBuilder(MainActivity.this);
+                builder.setTitle("Alert Dialog");
+                builder.setMessage("This is a simple Alert Dialog");
+                builder.setIcon(R.drawable.ic_msg);
+                builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                    }
+                });
+                builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+}
